bf55fffcbd25b7f9a5c8097f57a062886433e45a
Joe <zielinski@fastmail.com>
2021-07-08 15:57:04 +0200

    starting riscv compiler

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 extern int entry() asm("start");
 
 int main(int argc, char** argv) { 
-  long result = start();
-  printf("res: %ld \n", result);
+  char result = start();
+  printf("res: %c \n", result);
   return 0;
 }
